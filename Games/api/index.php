ab7cffb9f1a19dcae3694802b39a389c18a9f307
lucaspagliari <lucas.alex.pagliari@gmail.com>
2019-11-24 22:42:54 -0300

    Organize

--- Games/api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    echo "hello world";
-}
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    echo "hello people";
-} 
-
-?>
---
+++ README.md
@@ -0,0 +1,2 @@
+# Loja de jogos
+Aplicação desenvolvida para matéria de linguagem de programação e banco de dados
